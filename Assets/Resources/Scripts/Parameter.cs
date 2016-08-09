c091c7f1eff5c389972f70b3a70f2b1d439b8b70
Sonat Karan <sonat.karan@infosfer.com>
2016-08-09 09:54:54 +0300

    Refactor

--- Assets/Resources/Scripts/Parameter.cs
+++ Assets/Resources/Scripts/Parameter.cs
@@ -4,7 +4,7 @@ using UnityEngine.UI;
 
 public class Parameter : MonoBehaviour
 {
-    private ParameterData _parameter;
+    public ParameterData ParameterData;
 
 	// Use this for initialization
 	void Start () {
@@ -16,32 +16,27 @@ public class Parameter : MonoBehaviour
 	
 	}
 
-    public void SetParameter(ParameterData data)
-    {
-        _parameter = data;
-    }
-
     public void Refresh()
     {
         //InGame
         var InGameCheckBox = transform.FindChild("TitleField/Toggle").GetComponent<Toggle>();
-        InGameCheckBox.isOn = _parameter.inGame;
-        _parameter.Gadget.GetComponent<GadgetController>().Refresh();
+        InGameCheckBox.isOn = ParameterData.inGame;
+        ParameterData.Gadget.GetComponent<GadgetController>().Refresh();
 
         //Current
         var currentInputField = transform.FindChild("Variables/Current/Config/InputField").GetComponent<InputField>();
-        currentInputField.text = _parameter.current.ToString();
+        currentInputField.text = ParameterData.current.ToString();
 
         //Variance
         var varianceInputField = transform.FindChild("Variables/Variance/Config/InputField").GetComponent<InputField>();
-        varianceInputField.text = _parameter.variance.ToString();
+        varianceInputField.text = ParameterData.variance.ToString();
 
         //Minimum
         var minInputField = transform.FindChild("Variables/Minimum/Config/InputField").GetComponent<InputField>();
-        minInputField.text = _parameter.min.ToString();
+        minInputField.text = ParameterData.min.ToString();
 
         //Maximum
         var maxInputField = transform.FindChild("Variables/Maximum/Config/InputField").GetComponent<InputField>();
-        maxInputField.text = _parameter.max.ToString();
+        maxInputField.text = ParameterData.max.ToString();
     }
 }
