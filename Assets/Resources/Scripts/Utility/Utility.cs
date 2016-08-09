04cd17317e733043e9a56d27d496b1ddb43c5d17
Sonat Karan <sonat.karan@infosfer.com>
2016-08-09 09:54:32 +0300

    DeepCopy feature added.

---
+++ Assets/Resources/Scripts/Utility/Utility.cs
@@ -0,0 +1,22 @@
+﻿using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+using Newtonsoft.Json;
+
+public static class Utility
+{
+    public static T DeepCopy<T>(this T obj)
+    {
+        using (var stream = new MemoryStream())
+        {
+            var formatter = new BinaryFormatter();
+            formatter.Serialize(stream, obj);
+            stream.Seek(0, SeekOrigin.Begin);
+            return (T) formatter.Deserialize(stream);
+        }
+    }
+
+    public static T DeepCopyJson<T>(this T obj)
+    {
+        return JsonConvert.DeserializeObject<T>(JsonConvert.SerializeObject(obj));
+    }
+}
